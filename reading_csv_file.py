1904e1a9d2d13993587e6495dfe792d10c433205
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-19 21:40:51 +0800

    second approach

--- reading_csv_file.py
+++ reading_csv_file.py
@@ -3,7 +3,7 @@
 import csv
 
 # ========= PSEUDO CODE =========
-# This approach is without assigning a header, which it will automatically have a column name in the output/when printing
+# First Approach: This approach is without assigning a header, which it will automatically have a column name in the output/when printing
 # - Code for opening the original csv file
 with open('example1.csv', 'r') as file:
     sample_file = csv.reader(file)
@@ -11,6 +11,17 @@ with open('example1.csv', 'r') as file:
 # - Displaying the output using for loop
     for i in sample_file:
         print(i[0])
-        
 
-        
+
+print("---------------------------------------------------------------------------------------------------------------------------------------")
+# Second Approach: This approach is without assigning a header, which it will display an output of the first column content
+# - Code for opening the original csv file
+with open('example1.csv', 'r') as file:
+    sample_file2 = csv.reader(file)
+    
+    next(sample_file2)
+    
+# - Displaying the output using for loop
+    for i in sample_file2:
+        print(i[0])
+    
