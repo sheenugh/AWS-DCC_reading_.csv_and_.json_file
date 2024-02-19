acfa5af5c137927555f0759bca5923eb18c0bd54
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-19 23:16:45 +0800

    done

--- reading_json_file.py
+++ reading_json_file.py
@@ -3,7 +3,21 @@ import json
 
 # ========= PSEUDO CODE =========
 # - Code for opening the file
-# First Approach: In one line Only
+# First Approach: Printing in one line Only
 with open('example2.json', 'r') as file:
     sample_text = json.load(file)
     print(sample_text) # - Code for printing the content of the json file
+
+print("\n")
+print('-----------------------------')
+print("\n")
+
+# Second Approach: Printing what it is in the json file including the arrangement of datas.
+# - Code for opening the file
+with open('example2.json', 'r') as file:
+    sample = json.load(file)
+    
+# Code for printing the output
+    output = json.dumps(sample, indent=2) 
+    print(output)
+    
