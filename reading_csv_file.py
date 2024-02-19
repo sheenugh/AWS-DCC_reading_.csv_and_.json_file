407182e01879147445f83f3552b29ca288ddf162
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-19 21:37:18 +0800

    first approach

--- reading_csv_file.py
+++ reading_csv_file.py
@@ -1,6 +1,16 @@
 
 # ========== IMPORTS/S ==========
+import csv
 
 # ========= PSEUDO CODE =========
+# This approach is without assigning a header, which it will automatically have a column name in the output/when printing
 # - Code for opening the original csv file
-# - Displaying the output using for loop 
+with open('example1.csv', 'r') as file:
+    sample_file = csv.reader(file)
+
+# - Displaying the output using for loop
+    for i in sample_file:
+        print(i[0])
+        
+
+        
