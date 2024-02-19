2619801054fac52bfa557614054f23dab6949234
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-19 23:12:15 +0800

    first approach

--- reading_json_file.py
+++ reading_json_file.py
@@ -1,5 +1,9 @@
 # ========= IMPORT/S =========
+import json
 
 # ========= PSEUDO CODE =========
 # - Code for opening the file
-# - Code for printing the content of the json file
+# First Approach: In one line Only
+with open('example2.json', 'r') as file:
+    sample_text = json.load(file)
+    print(sample_text) # - Code for printing the content of the json file
